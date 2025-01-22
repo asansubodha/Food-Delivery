8726d014077685f4489df4801df2ed5b056ada64
R.M Asan Subodha Madusankha <ashansubodha8@gmail.com>
2025-01-22 17:02:51 +0530

    silver app bar

--- foody/lib/pages/home_page.dart
+++ foody/lib/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:foody/components/current_location.dart';
+import 'package:foody/components/description_box.dart';
 import 'package:foody/components/drawer.dart';
+import 'package:foody/components/silver_app_bar.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -12,12 +15,32 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home Page"),
-        backgroundColor: Theme.of(context).colorScheme.surface,
-        
-      ),
       drawer: MyDrawer(),
+      body: NestedScrollView(
+        headerSliverBuilder: (context, innerBoxIsScrolled) => [
+          MySilverAppBar(
+            title: Text("title"),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Divider(
+                  indent: 25,
+                  endIndent: 25,
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+                //my current location
+                const CurrentLocation(),
+
+                //description box
+                const DescriptionBox(),
+              ],
+            ),
+          ),
+        ],
+        body: Container(
+          color: Colors.blue,
+        ),
+      ),
     );
   }
 }
